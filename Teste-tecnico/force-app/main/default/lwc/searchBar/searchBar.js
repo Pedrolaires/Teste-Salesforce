1262684ad6041ab91e2e13fa64d0ce4352c41c33
Pedro Aires <pedro.santos.1212436@sga.pucminas.br>
2025-11-02 12:33:25 -0300

    chore: removendo debugs de desenvolvimento

--- Teste-tecnico/force-app/main/default/lwc/searchBar/searchBar.js
+++ Teste-tecnico/force-app/main/default/lwc/searchBar/searchBar.js
@@ -14,7 +14,6 @@ export default class SearchBar extends LightningElement {
         const searchKey = event.target.value;
         window.clearTimeout(this.delayTimeout);
         this.delayTimeout = window.setTimeout(() => {
-            console.log('valor sdo srch:', searchKey);
             this.dispatchEvent(new CustomEvent('search', {
                 detail: { value: searchKey }
             }));
